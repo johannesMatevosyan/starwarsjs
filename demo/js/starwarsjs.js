d25de557f0c20c278bc55a014eff7aeecfe52aed
johannesMatevosyan <h.matevosyan@yahoo.com>
2016-09-04 07:35:03 -0700

    improve coding styles

--- demo/js/starwarsjs.js
+++ demo/js/starwarsjs.js
@@ -9,7 +9,6 @@ $( document ).ready(function() {
             var starwarsjs = {
                 target: settings.target
             };
-
             traverse(starwarsjs);
             presentation(starwarsjs);
             return this;
@@ -21,7 +20,6 @@ $( document ).ready(function() {
 function traverse(starwarsjs){
 
     $(starwarsjs.target).each(function(){
-
         $(this).hover( // change star's color after mouse hover
             function() {
                 $(this).prevAll().andSelf().addClass('over');
@@ -30,17 +28,12 @@ function traverse(starwarsjs){
                 $(this).prevAll().andSelf().removeClass('over');
             }
         );
-
         $(this).on('click', function(){ // get rate after click
-
             var star_id = $(this).attr('data-id');
             $(this).siblings('input.get_star').val(star_id);
             $(this).addClass('checked');
             $(this).prevAll().addClass('checked');
             $(this).nextAll().removeClass('checked');
-
-            // check if numbers are set true
-
         });
     });
 
@@ -48,20 +41,16 @@ function traverse(starwarsjs){
 
 
 function presentation(){
-
     $("#change_fonts").bind('keyup mouseup', function () {
         console.log($(this).val());
         $('.rate_star').css({ "fontSize": $(this).val() + "px"})
     });
-
     $("#change_color").on('change', function () {
         var color = $(this).val();
         $("<style type='text/css'> .rate_star{ color:" + color + ";} </style>").appendTo("head");
     });
-
     $("#change_hover_color").on('change', function () {
         var hover_color = $(this).val();
         $("<style type='text/css'> .rate_star.over{ color:" + hover_color + ";} </style>").appendTo("head");
     });
-
 }
